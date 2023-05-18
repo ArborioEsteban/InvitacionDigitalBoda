ee185e003a55448a905449aff20813b7a3f8befc
Esteban Arborio <estebanarborio@gmail.com>
2023-05-18 19:40:11 -0300

    Temas de diseño de letras

--- src/components/Confirmation/Confirmation.jsx
+++ src/components/Confirmation/Confirmation.jsx
@@ -4,9 +4,14 @@ import './Confirmation.css'
 const Confirmation = () => {
   return (
     <>
-        <p id='textConfirmation'>Como llegar?</p>
-        <p id='textConfirmation'><a href='https://docs.google.com/forms/d/e/1FAIpQLSeJMeHMvm0CBf4MuK7lzsrAc-lFVG7VlGfUSCO-0VRUus0-8A/viewform?usp=sf_link' target='blankgit '>Confirmar Asistencia Aqui</a></p>
+        <p id='textConfirmation'><a href='https://docs.google.com/forms/d/e/1FAIpQLSeJMeHMvm0CBf4MuK7lzsrAc-lFVG7VlGfUSCO-0VRUus0-8A/viewform?usp=sf_link' target='blankgit ' className='text-decoration-none'>Confirmar Asistencia Aqui</a>
+        </p>
+
+        <p>Salon Danubio - Ruta 315 Km 9 - Tafi viejo </p>
+        <p id='textConfirmation'><a href='https://goo.gl/maps/1B2gdcBF8oArTeuD7' target='blankgit ' className='text-decoration-none'>Como llegar?</a ></p>
+        
     </>
+    
   )
 }
 
--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -5,12 +5,17 @@ const Welcome = () => {
   return (
     <>
     <div className='imagenContendeor'>
-        <img src="../../../public/letras.png" alt="EmanuelYNancy" className='text-center imagenNombres'/>
-        {/* <p className='title'>NANCY</p>
-        <p className='title'>Y</p>
-        <p className='title'>EMMANUEL</p>
-        <h2 className='fs-1'>Nos casamos!</h2>
-        <h3 className='fs-1'>30/6/23 - 22hs</h3> */}
+        {/* <img src="../../../public/letras.png" alt="EmanuelYNancy" className='text-center imagenNombres'/> */}
+        <h6 className='Subtitles pt-5 mt-5'>Guarda la Fecha</h6>
+        <p id="textCounter">Viernes 30/06/2023</p>
+        <p className='title mt-5'>Emmanuel Y Nancy</p>
+        {/* <p className='title'>Y</p>
+        <p className='title'>Nancy</p> */}
+        
+        {/* <h3 className='fs-1 Subtitles'>30/6/23 - 22hs</h3>
+        <h4 className='fs-1 Subtitles'>Salon Danubio <br></br> Ruta 315
+Km 9<br></br>
+Tafi viejo</h4> */}
     </div>
     </>
   )
